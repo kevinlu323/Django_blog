bf5547adc7ad16ab04e5b5a50248ba4d43cd8e06
Linkui Lu <kevinlu323@gmail.com>
2018-02-04 04:55:45 +0000

    Update models for blogger app

--- blogger/models.py
+++ blogger/models.py
@@ -1,20 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
+
 
 # Create your models here.
 
-class User(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=30)
-    email = models.CharField(max_length=50)
-    avatar = models.CharField(max_length=100)
-    is_admin = models.BooleanField(default=False)
-    created_date = models.DateTimeField(auto_now=True)
-
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
-    summary = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     posted_date = models.DateTimeField(auto_now=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     is_hidden = models.BooleanField(default=False)
     allow_comment = models.BooleanField(default=True)
 
@@ -30,8 +24,4 @@ class Comment(models.Model):
     id = models.AutoField(primary_key=True)
     content = models.TextField()
     post_date = models.DateTimeField(auto_now=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-
-
-
-
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
