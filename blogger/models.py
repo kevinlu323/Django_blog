eea48ef8fa374429bf34ae671a588a5895e40e3c
Linkui Lu <kevinlu323@gmail.com>
2018-02-05 21:38:35 +0000

    Another comit for models modification

--- blogger/models.py
+++ blogger/models.py
@@ -6,6 +6,9 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=20, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
@@ -17,6 +20,10 @@ class Post(models.Model):
     is_hidden = models.BooleanField(default=False)
     allow_comment = models.BooleanField(default=True)
 
+    def __str__(self):
+        shortTitle = self.title[:40] + "..." if len(self.title) > 40 else self.title
+        return shortTitle
+
 
 class Comment(models.Model):
     id = models.AutoField(primary_key=True)
@@ -24,3 +31,7 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     posted_date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        shortContent = self.content[:40] + "..." if len(self.content) > 40 else self.content
+        return shortContent
