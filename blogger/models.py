599f2ec008dda4c08596a8f0d090ee582f34deda
Linkui Lu <kevinlu323@gmail.com>
2018-02-05 21:12:24 +0000

    Continue modifying models for blogger app

--- blogger/models.py
+++ blogger/models.py
@@ -3,25 +3,24 @@ from django.db import models
 
 
 # Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+
 
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
+    content = models.TextField()
     posted_date = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, blank=True)
     is_hidden = models.BooleanField(default=False)
     allow_comment = models.BooleanField(default=True)
 
 
-class PostDetail(models.Model):
-    id = models.AutoField(primary_key=True)
-    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
-    sequence = models.PositiveIntegerField(default=0)
-    content = models.TextField()
-
-
 class Comment(models.Model):
     id = models.AutoField(primary_key=True)
-    content = models.TextField()
-    post_date = models.DateTimeField(auto_now=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    posted_date = models.DateTimeField(auto_now=True)
