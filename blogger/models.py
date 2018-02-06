1e1647f7c4e8dd76a4a154145819b5716c9c634a
Linkui Lu <kevinlu323@gmail.com>
2018-02-06 05:08:11 +0000

    Update models to add Category model

--- blogger/models.py
+++ blogger/models.py
@@ -4,7 +4,16 @@ from django.db import models
 
 # Create your models here.
 class Tag(models.Model):
-    name = models.CharField(max_length=20, unique=True)
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=40, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=40, unique=True)
 
     def __str__(self):
         return self.name
@@ -14,8 +23,9 @@ class Post(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     content = models.TextField()
-    posted_date = models.DateTimeField(auto_now=True)
+    created_time = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
     tags = models.ManyToManyField(Tag, blank=True)
     is_hidden = models.BooleanField(default=False)
     allow_comment = models.BooleanField(default=True)
@@ -30,8 +40,8 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    posted_date = models.DateTimeField(auto_now=True)
-    
+    created_time = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         shortContent = self.content[:40] + "..." if len(self.content) > 40 else self.content
         return shortContent
