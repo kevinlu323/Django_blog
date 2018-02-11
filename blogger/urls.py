f7db2032f0829d13b99b487551fa56a8e096592a
Linkui Lu <kevinlu323@gmail.com>
2018-02-11 22:15:05 +0000

    Add blogger index and detail page

--- blogger/urls.py
+++ blogger/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, re_path
 from . import views
 
+app_name = "blogger"
+
 urlpatterns = [
     re_path(r'^$', views.index, name='index'),
-    path('index.html', views.index, name='index'),
+    path('post/tmp/<int:pk>/', views.tmpDetail, name='tmpDetail'),
+    path('post/<int:post_id>/', views.detail, name='detail'),
+    path('tmp', views.tmpDetail, name='tmpDetail'),
 ]
--- blogger/views.py
+++ blogger/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
-from .models import Post
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from .models import Post, Comment
 
 # Create your views here.
 
 def index(request):
     post_list = Post.objects.all().order_by('-created_time')
     return render(request, 'blogger/index.html', context={'post_list': post_list})
+
+def tmpDetail(request, pk=None):
+    return render(request, 'blogger/single_new.html')
+
+def detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    #comment_list = Comment.objects.filter(post=post_id).order_by('-created_time')
+    comment_list = post.comment_set.all().order_by('-created_time')
+    return render(request, 'blogger/detail.html', context={'post': post, 'comment_list': comment_list})
